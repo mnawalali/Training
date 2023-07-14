ed91b0e484373011c835d2eac65d4de3344c29d6
mnawalali <139316387+mnawalali@users.noreply.github.com>
2023-07-14 10:46:38 +0500

    weather man init

--- weather.rb
+++ weather.rb
@@ -1,9 +1,15 @@
-folder_names = Dir.glob('Data/*').select { |entry| File.directory?(entry) }
-file_names = []
+# folder_names = Dir.glob('Data/*').select { |entry| File.directory?(entry) }
+# file_names = []
 
-puts folder_names
+# print folder_names
 
-folder_names.each do |path|
-  file_names.push(%x|ls #{path}|) 
-#   p file_names, " "
+# folder_names.each do |path|
+#   file_names.push(%x|ls #{path}|) 
+# #   p file_names, " "
+# end
+
+def get_year_info ()
+  fileobject = File.new("Murree_weather_2004_Aug.txt", "r")
+  File.open("sample.txt", "r"); 
+  File.open.each do
 end
